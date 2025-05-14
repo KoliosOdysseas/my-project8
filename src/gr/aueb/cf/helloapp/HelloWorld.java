94f1cb22c53eed7f983a81056d02f8874dd75bea
kolios.odysseas <kolios.odysseas@gmail.com>
2025-05-14 20:47:13 +0300

    feat: add hello world

---
+++ src/gr/aueb/cf/helloapp/HelloWorld.java
@@ -0,0 +1,11 @@
+package gr.aueb.cf.helloapp;
+
+/**
+ * this project created to show how
+ * to make a git project. and then how
+ * to stage and commit changes in local repo.
+ * and then how to push to gitHub
+ */
+
+public class HelloWorld {
+}
